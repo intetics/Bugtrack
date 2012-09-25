6588978dd50f40661b063f52650fe0d42beb42e4
Ilia Akgaev <twealm@gmail.com>
2012-09-25 16:48:32 +0300

    Add empty project

---
+++ JIRA/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  JIRA
+//
+//  Created by Ilia Akgaev on 9/25/12.
+//  Copyright (c) 2012 Intetics Co. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ JIRA/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  JIRA
+//
+//  Created by Ilia Akgaev on 9/25/12.
+//  Copyright (c) 2012 Intetics Co. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
