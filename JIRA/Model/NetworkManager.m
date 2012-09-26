2839066773e2c16c053d730f6f82576269290f3c
Ilia Akgaev <twealm@gmail.com>
2012-09-26 15:23:04 +0300

    Add getAllIssuesForCurrentUser  method implementation
    getAllIssuesForCurrentUser method returns array of all issues assigned
    to user who logged in.

--- JIRA/Model/NetworkManager.m
+++ JIRA/Model/NetworkManager.m
@@ -9,6 +9,7 @@
 #import "NetworkManager.h"
 #import "AFNetworking.h"
 #import "config.h"
+#import "JSONKit.h"
 
 @interface NetworkManager ()
 @property (strong, nonatomic) AFHTTPClient * httpClient;
@@ -43,25 +44,32 @@
 
 //TODO: Make it more generic. Right now it uses assumption that we can pass auth. header in request.
 //FIXME:
-- (void) getAllIssuesForCurrentUser {
+- (NSArray *) getAllIssuesForCurrentUser {
     NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
     NSString *userName = [userDefaults objectForKey:@"username"];
     NSString *password = [userDefaults objectForKey:@"password"];
     
     [self.httpClient setAuthorizationHeaderWithUsername:userName password:password];
-    [self.httpClient setParameterEncoding:AFJSONParameterEncoding];
-    [self.httpClient registerHTTPOperationClass:[AFJSONRequestOperation class]];
     
-    NSMutableString *path = (NSMutableString *)@"search?jql=assignee%3D";
-    [path appendString:userName];
+    NSString *path = @"search?jql=assignee%3D";
+    NSMutableString *fullpath = [path mutableCopy];
+    [fullpath appendString:@"\%22"];
+    [fullpath appendString:userName];
+    [fullpath appendString:@"\%22"];
     
-    [self.httpClient getPath:path parameters:[NSDictionary dictionaryWithObject:@"application/json" forKey:@"Content-Type:"]
+    __block NSArray *response;
+    [self.httpClient getPath:fullpath parameters:[NSDictionary dictionaryWithObject:@"application/json" forKey:@"Content-Type"]
                      success:^(AFHTTPRequestOperation *operation, id responseObject){
-                         NSDictionary *response = (NSDictionary *)responseObject;
+                         
+                         JSONDecoder* decoder = [[JSONDecoder alloc]
+                                                 initWithParseOptions:JKParseOptionNone];
+                         response = [decoder objectWithData:responseObject];
                          NSLog(@"We get:%@", response);
                      }failure:^(AFHTTPRequestOperation *operation, NSError *error){
-                         
+                         NSLog(@"Error: %@", error);
+                         response = nil;
                      }];
+    return response;
 }
 
 @end
