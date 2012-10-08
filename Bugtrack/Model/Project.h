d2447528a35779a0d769dd004e086317955dc25b
Ilia Akgaev <twealm@gmail.com>
2012-10-08 12:24:14 +0300

    Add Project class for JSON mapping

---
+++ Bugtrack/Model/Project.h
@@ -0,0 +1,13 @@
+//
+//  Project.h
+//  Bugtrack
+//
+//  Created by Ilia Akgaev on 10/8/12.
+//  Copyright (c) 2012 Intetics Co. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Project : NSObject
+
+@end
---
+++ Bugtrack/Model/Project.m
@@ -0,0 +1,13 @@
+//
+//  Project.m
+//  Bugtrack
+//
+//  Created by Ilia Akgaev on 10/8/12.
+//  Copyright (c) 2012 Intetics Co. All rights reserved.
+//
+
+#import "Project.h"
+
+@implementation Project
+
+@end
