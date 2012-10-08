37ab8754c47bab95eab36edc0bb86f34228eaa31
Ilia Akgaev <twealm@gmail.com>
2012-10-08 19:28:11 +0300

    Add detail view for showing issue info

---
+++ Bugtrack/ViewControllers/DetailViewController.m
@@ -0,0 +1,38 @@
+//
+//  DetailViewController.m
+//  Bugtrack
+//
+//  Created by Ilia Akgaev on 10/8/12.
+//  Copyright (c) 2012 Intetics Co. All rights reserved.
+//
+
+#import "DetailViewController.h"
+
+@interface DetailViewController ()
+
+@end
+
+@implementation DetailViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
