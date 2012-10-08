825f55475e3f492d8f30f198d066438d7414f011
Ilia Akgaev <twealm@gmail.com>
2012-10-08 12:25:18 +0300

    Project: Add implementation

--- Bugtrack/Model/Project.h
+++ Bugtrack/Model/Project.h
@@ -9,5 +9,8 @@
 #import <Foundation/Foundation.h>
 
 @interface Project : NSObject
-
+@property (strong, nonatomic) NSString* key;
+@property (strong, nonatomic) NSString* name;
+@property (strong, nonatomic) NSString* link;
+@property (strong, nonatomic) NSArray* issues;
 @end
--- Bugtrack/Model/Project.m
+++ Bugtrack/Model/Project.m
@@ -10,4 +10,10 @@
 
 @implementation Project
 
+#pragma mark - NSObject method reloading
+- (NSString*) description {
+    NSString *description = [NSString stringWithFormat:@"Project: \nName: %@ \n Key:%@ \n Link:%@ \n Issues count: %u" , self.name, self.key, self.link, [self.issues count]];
+    return description;
+}
+
 @end
